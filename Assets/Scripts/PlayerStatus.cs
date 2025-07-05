d66bd1e7df61002e5ebefe1e97f5685516fd2833
Antti Moilanen <anttimo@gmail.com>
2025-07-05 13:48:09 +0300

    Add mutation sound (and convert some tabs to spaces it seems..)

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -2,222 +2,233 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[CreateAssetMenu(fileName = "PlayerStatus", menuName = "jam-oceangt/Player Status")]
+public class PlayerStatus : ScriptableObject
+{
+    [Header("Oxygen Settings")]
+    [SerializeField]
+    private float maxOxygen = 100f;
 
-    [CreateAssetMenu(fileName = "PlayerStatus", menuName = "jam-oceangt/Player Status")]
-    public class PlayerStatus : ScriptableObject
-    {
-        [Header("Oxygen Settings")]
-        [SerializeField]
-        private float maxOxygen = 100f;
+    [SerializeField]
+    private float currentOxygen;
 
-        [SerializeField]
-        private float currentOxygen;
+    [Header("Sanity Settings")]
+    [SerializeField]
+    private float maxSanity = 100f;
 
-        [Header("Sanity Settings")]
-        [SerializeField]
-        private float maxSanity = 100f;
+    [SerializeField]
+    private float currentSanity;
 
-        [SerializeField]
-        private float currentSanity;
+    [Header("Light settings")]
+    [SerializeField]
+    private float maxLightPower = 100f;
 
-        [Header("Light settings")]
-        [SerializeField]
-        private float maxLightPower = 100f;
-        [SerializeField]
-        private float currentLightPower;
+    [SerializeField]
+    private float currentLightPower;
 
-        [Header("Status")]
-        [SerializeField]
-        private bool isDead = false;
-        
-        [SerializeField]
-        private int fishEatCount = 0;
+    [Header("Status")]
+    [SerializeField]
+    private bool isDead = false;
 
-        private int requiredFishEatCount = 30; // Number of fish to eat to win
-        
-        private int[] mutationThresholds = new int[] { 5, 10, 15, 20, 25, 30 }; // Thresholds for mutations};
+    [SerializeField]
+    private int fishEatCount = 0;
 
-        [SerializeField]
-        private int currentMutationIndex = 0;
-        // Events
+    private int requiredFishEatCount = 30; // Number of fish to eat to win
+
+    private int[] mutationThresholds = new int[] { 5, 10, 15, 20, 25, 30 }; // Thresholds for mutations};
+
+    [SerializeField]
+    private int currentMutationIndex = 0;
+
+    // Events
     public event Action<float> OnOxygenChanged;
-        public event Action<float> OnSanityChanged;
-        public event Action<float> OnLightPowerChanged;
+    public event Action<float> OnSanityChanged;
+    public event Action<float> OnLightPowerChanged;
 
-        public event Action<int> OnTriggerMutation;
+    public event Action<int> OnTriggerMutation;
 
-        public event Action OnPlayerDied;
-        
-        public event Action OnPlayerWon;
+    public event Action OnPlayerDied;
 
-        // Properties
+    public event Action OnPlayerWon;
+
+    // Properties
     public float MaxOxygen => maxOxygen;
-        public float CurrentOxygen => currentOxygen;
-        public float MaxSanity => maxSanity;
-        public float CurrentSanity => currentSanity;
-        public float CurrentLightPower => currentLightPower;
-        public bool IsDead => isDead;
-        public bool isImmortal = false;
+    public float CurrentOxygen => currentOxygen;
+    public float MaxSanity => maxSanity;
+    public float CurrentSanity => currentSanity;
+    public float CurrentLightPower => currentLightPower;
+    public bool IsDead => isDead;
+    public bool isImmortal = false;
 
-        // Normalized health value (0-1)
+    // Normalized health value (0-1)
     private float oxygenPercentage => Mathf.Clamp01(currentOxygen / maxOxygen);
-        private float sanityPercentage => Mathf.Clamp01(currentSanity / maxSanity);
+    private float sanityPercentage => Mathf.Clamp01(currentSanity / maxSanity);
 
-        // Reset status when scene is reloaded
-        private void OnSceneLoaded(
-            UnityEngine.SceneManagement.Scene scene,
-            UnityEngine.SceneManagement.LoadSceneMode mode
-        )
+    // Reset status when scene is reloaded
+    private void OnSceneLoaded(
+        UnityEngine.SceneManagement.Scene scene,
+        UnityEngine.SceneManagement.LoadSceneMode mode
+    )
+    {
+        ResetStatus();
+    }
+
+    private void OnEnable()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+        ResetStatus();
+    }
+
+    private void OnDisable()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    // Reset player status to default values
+    public void ResetStatus()
+    {
+        Debug.Log("Resetting player status");
+        currentOxygen = maxOxygen;
+        currentSanity = maxSanity;
+        currentLightPower = 15f;
+        isDead = false;
+        fishEatCount = 0;
+        currentMutationIndex = 0;
+        isImmortal = false;
+        OnOxygenChanged?.Invoke(oxygenPercentage);
+        OnSanityChanged?.Invoke(sanityPercentage);
+        OnLightPowerChanged?.Invoke(currentLightPower);
+    }
+
+    public void ApplyEffect(FishEffectType fishEffectType)
+    {
+        switch (fishEffectType)
         {
-            ResetStatus();
+            case FishEffectType.Light:
+                IncreaseLightPower(50);
+                break;
+            case FishEffectType.Sanity:
+                GainSanity(10);
+                break;
+            case FishEffectType.Oxygen:
+                GainOxygen(12);
+                break;
+            case FishEffectType.Predator:
+                LoseSanity(20);
+                DepleteOxygen(20);
+                break;
         }
-
-        private void OnEnable()
+        fishEatCount++;
+        Debug.Log(
+            $"Fish eaten: {fishEatCount} "
+                + mutationThresholds[0]
+                + " "
+                + mutationThresholds[1]
+                + " "
+                + mutationThresholds[2]
+        );
+        for (int i = 0; i < mutationThresholds.Length; i++)
         {
-            UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
-            ResetStatus();
-        }
-
-        private void OnDisable()
-        {
-            UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
-        }
-
-        private void OnDestroy()
-        {
-            UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
-        }
-
-        // Reset player status to default values
-        public void ResetStatus()
-        {
-            Debug.Log("Resetting player status");
-            currentOxygen = maxOxygen;
-            currentSanity = maxSanity;
-            currentLightPower = 15f;
-            isDead = false;
-            fishEatCount = 0;
-            currentMutationIndex = 0;
-            isImmortal = false;
-            OnOxygenChanged?.Invoke(oxygenPercentage);
-            OnSanityChanged?.Invoke(sanityPercentage);
-            OnLightPowerChanged?.Invoke(currentLightPower);
-        }
-
-        public void ApplyEffect(FishEffectType fishEffectType) {
-            switch (fishEffectType) {
-                case FishEffectType.Light:
-                    IncreaseLightPower(50);
-                    break;
-                case FishEffectType.Sanity:
-                    GainSanity(10);
-                    break;
-                case FishEffectType.Oxygen:
-                    GainOxygen(12);
-                    break;
-                case FishEffectType.Predator:
-                    LoseSanity(20);
-                    DepleteOxygen(20);
-                    break;
-            }
-            fishEatCount++;
-            Debug.Log($"Fish eaten: {fishEatCount}");
-            for(int i = 0; i < mutationThresholds.Length; i++) {
             if (fishEatCount == mutationThresholds[i])
             {
                 currentMutationIndex++;
                 OnTriggerMutation?.Invoke(currentMutationIndex); // Trigger mutation event
-                Debug.Log($"Triggered mutation {currentMutationIndex} at fish count {fishEatCount}");
-
-            }
-            }
-            if (fishEatCount >= requiredFishEatCount) {
-                Debug.Log("Player has eaten enough fish to win!");
-                isImmortal = true; // Set player to immortal state
-                OnPlayerWon?.Invoke();
-            }
-            
-            
-        }
-
-        public void OnPredatorHit() {
-            LoseSanity(20);
-            DepleteOxygen(20);
-        }
-
-        public void IncreaseLightPower(float lightPowerAmount)
-        {
-            currentLightPower = Mathf.Min(maxLightPower, currentLightPower + lightPowerAmount);
-            OnLightPowerChanged?.Invoke(currentLightPower);
-        }
-
-        public void DepleteOxygen(float oxygenAmount)
-        {
-            if (isDead || isImmortal)
-                return;
-
-            currentOxygen = Mathf.Max(0, currentOxygen - oxygenAmount);
-            OnOxygenChanged?.Invoke(oxygenPercentage);
-
-            // Check for death
-            if (currentOxygen <= 0 && !isDead)
-            {
-                SetDead(true);
+                Debug.Log(
+                    $"Triggered mutation {currentMutationIndex} at fish count {fishEatCount}"
+                );
             }
         }
-
-        public void GainOxygen(float oxygenAmount)
+        if (fishEatCount >= requiredFishEatCount)
         {
-            currentOxygen = Mathf.Min(maxOxygen, currentOxygen + oxygenAmount);
-            OnOxygenChanged?.Invoke(oxygenPercentage);
-        }
-
-        public void LoseSanity(float sanityAmount)
-        {
-            Debug.Log("Losing sanity: " + sanityAmount);
-            if (isDead || isImmortal)
-                return;
-
-            currentSanity = Mathf.Max(0, currentSanity - sanityAmount);
-            OnSanityChanged?.Invoke(sanityPercentage);
-
-            // Check for death
-            if (currentSanity <= 0 && !isDead)
-            {
-                Debug.Log("Player is dead");
-                SetDead(true);
-            }
-        }
-
-        public void GainSanity(float sanityAmount)
-        {
-            currentSanity = Mathf.Min(maxSanity, currentSanity + sanityAmount);
-            OnSanityChanged?.Invoke(sanityPercentage);
-        }
-
-        // Heal the player
-        public void Heal(float healAmount)
-        {
-            if (isDead)
-                return; // Can't heal if dead
-
-            currentOxygen = Mathf.Min(maxOxygen, currentOxygen + healAmount);
-            OnOxygenChanged?.Invoke(oxygenPercentage);
-        }
-
-        // Set player's dead state
-        public void SetDead(bool dead)
-        {
-            Debug.Log("Setting player dead state to: " + dead);
-            if (isDead == dead)
-                return; // No change
-
-            isDead = dead;
-
-            if (isDead)
-            {
-                OnPlayerDied?.Invoke();
-            }
+            Debug.Log("Player has eaten enough fish to win!");
+            isImmortal = true; // Set player to immortal state
+            OnPlayerWon?.Invoke();
         }
     }
 
+    public void OnPredatorHit()
+    {
+        LoseSanity(20);
+        DepleteOxygen(20);
+    }
+
+    public void IncreaseLightPower(float lightPowerAmount)
+    {
+        currentLightPower = Mathf.Min(maxLightPower, currentLightPower + lightPowerAmount);
+        OnLightPowerChanged?.Invoke(currentLightPower);
+    }
+
+    public void DepleteOxygen(float oxygenAmount)
+    {
+        if (isDead || isImmortal)
+            return;
+
+        currentOxygen = Mathf.Max(0, currentOxygen - oxygenAmount);
+        OnOxygenChanged?.Invoke(oxygenPercentage);
+
+        // Check for death
+        if (currentOxygen <= 0 && !isDead)
+        {
+            SetDead(true);
+        }
+    }
+
+    public void GainOxygen(float oxygenAmount)
+    {
+        currentOxygen = Mathf.Min(maxOxygen, currentOxygen + oxygenAmount);
+        OnOxygenChanged?.Invoke(oxygenPercentage);
+    }
+
+    public void LoseSanity(float sanityAmount)
+    {
+        Debug.Log("Losing sanity: " + sanityAmount);
+        if (isDead || isImmortal)
+            return;
+
+        currentSanity = Mathf.Max(0, currentSanity - sanityAmount);
+        OnSanityChanged?.Invoke(sanityPercentage);
+
+        // Check for death
+        if (currentSanity <= 0 && !isDead)
+        {
+            Debug.Log("Player is dead");
+            SetDead(true);
+        }
+    }
+
+    public void GainSanity(float sanityAmount)
+    {
+        currentSanity = Mathf.Min(maxSanity, currentSanity + sanityAmount);
+        OnSanityChanged?.Invoke(sanityPercentage);
+    }
+
+    // Heal the player
+    public void Heal(float healAmount)
+    {
+        if (isDead)
+            return; // Can't heal if dead
+
+        currentOxygen = Mathf.Min(maxOxygen, currentOxygen + healAmount);
+        OnOxygenChanged?.Invoke(oxygenPercentage);
+    }
+
+    // Set player's dead state
+    public void SetDead(bool dead)
+    {
+        Debug.Log("Setting player dead state to: " + dead);
+        if (isDead == dead)
+            return; // No change
+
+        isDead = dead;
+
+        if (isDead)
+        {
+            OnPlayerDied?.Invoke();
+        }
+    }
+}
--- Assets/Scripts/PlayerStatusController.cs
+++ Assets/Scripts/PlayerStatusController.cs
@@ -4,32 +4,65 @@ using UnityEngine.Rendering.Universal;
 
 public class PlayerStatusController : MonoBehaviour
 {
+    [SerializeField]
+    private PlayerStatus playerStatus;
 
-    [SerializeField] private PlayerStatus playerStatus;
-    [SerializeField] private float sanityDepletionRate = 5f;
-    [SerializeField] private float oxygenDepletionRate = 5f;
-    [SerializeField] private AudioSource playerAudioSource;
-    [SerializeField] private AudioClip[] predatorHitClips = new AudioClip[3];
-    [SerializeField] private AudioClip drowningSound;
+    [SerializeField]
+    private float sanityDepletionRate = 5f;
 
-    [SerializeField] private Sprite leftMutationSprite;
-    [SerializeField] private Sprite armMutationSprite;
-    [SerializeField] private Sprite headMutationSprite;
-    [SerializeField] private Sprite bodyMutationSprite;
+    [SerializeField]
+    private float oxygenDepletionRate = 5f;
 
-    [SerializeField] private SpriteRenderer headSpriteRenderer;
-    [SerializeField] private SpriteRenderer bodySpriteRenderer;
-    [SerializeField] private SpriteRenderer leftLegSpriteRenderer;
-    [SerializeField] private SpriteRenderer rightLegSpriteRenderer;
-    [SerializeField] private SpriteRenderer leftArmSpriteRenderer;
-    [SerializeField] private SpriteRenderer rightArmSpriteRenderer;
+    [SerializeField]
+    private AudioSource playerAudioSource;
 
-    [SerializeField] private float maxEyeLightIntensity = 2f;
+    [SerializeField]
+    private AudioClip[] predatorHitClips = new AudioClip[3];
 
-    [SerializeField] private Light2D eyeLight1;
-    [SerializeField] private Light2D eyeLight2;
+    [SerializeField]
+    private AudioClip drowningSound;
 
+    [SerializeField]
+    private AudioClip mutationAudioClip;
 
+    [SerializeField]
+    private Sprite leftMutationSprite;
+
+    [SerializeField]
+    private Sprite armMutationSprite;
+
+    [SerializeField]
+    private Sprite headMutationSprite;
+
+    [SerializeField]
+    private Sprite bodyMutationSprite;
+
+    [SerializeField]
+    private SpriteRenderer headSpriteRenderer;
+
+    [SerializeField]
+    private SpriteRenderer bodySpriteRenderer;
+
+    [SerializeField]
+    private SpriteRenderer leftLegSpriteRenderer;
+
+    [SerializeField]
+    private SpriteRenderer rightLegSpriteRenderer;
+
+    [SerializeField]
+    private SpriteRenderer leftArmSpriteRenderer;
+
+    [SerializeField]
+    private SpriteRenderer rightArmSpriteRenderer;
+
+    [SerializeField]
+    private float maxEyeLightIntensity = 2f;
+
+    [SerializeField]
+    private Light2D eyeLight1;
+
+    [SerializeField]
+    private Light2D eyeLight2;
 
     private void OnEnable()
     {
@@ -42,12 +75,10 @@ public class PlayerStatusController : MonoBehaviour
         playerStatus.OnTriggerMutation -= OnTriggerMutation;
         playerStatus.OnPlayerDied -= OnPlayerDied;
     }
-    
+
     private void Update()
     {
-
-        if (GameManager.Instance == null ||
-            GameManager.Instance.currentGameState != GameState.Game)
+        if (GameManager.Instance == null || GameManager.Instance.currentGameState != GameState.Game)
             return;
         playerStatus.DepleteOxygen(oxygenDepletionRate * Time.deltaTime);
         playerStatus.LoseSanity(sanityDepletionRate * Time.deltaTime);
@@ -61,7 +92,10 @@ public class PlayerStatusController : MonoBehaviour
     public void OnPredatorHit()
     {
         playerStatus.OnPredatorHit();
-        playerAudioSource.PlayOneShot(predatorHitClips[Random.Range(0, predatorHitClips.Length)], 0.7f);
+        playerAudioSource.PlayOneShot(
+            predatorHitClips[Random.Range(0, predatorHitClips.Length)],
+            0.7f
+        );
     }
 
     private void OnTriggerMutation(int mutationIndex)
@@ -72,21 +106,27 @@ public class PlayerStatusController : MonoBehaviour
         {
             case 1:
                 bodySpriteRenderer.sprite = bodyMutationSprite;
+                playerAudioSource.PlayOneShot(mutationAudioClip);
                 break;
             case 2:
                 leftLegSpriteRenderer.sprite = leftMutationSprite;
+                playerAudioSource.PlayOneShot(mutationAudioClip);
                 break;
             case 3:
                 leftArmSpriteRenderer.sprite = armMutationSprite;
+                playerAudioSource.PlayOneShot(mutationAudioClip);
                 break;
             case 4:
                 rightArmSpriteRenderer.sprite = armMutationSprite;
+                playerAudioSource.PlayOneShot(mutationAudioClip);
                 break;
             case 5:
                 rightLegSpriteRenderer.sprite = leftMutationSprite;
+                playerAudioSource.PlayOneShot(mutationAudioClip);
                 break;
             case 6:
                 headSpriteRenderer.sprite = headMutationSprite;
+                playerAudioSource.PlayOneShot(mutationAudioClip);
                 break;
             default:
                 Debug.LogWarning("Unknown mutation index: " + mutationIndex);
@@ -98,10 +138,15 @@ public class PlayerStatusController : MonoBehaviour
     private void UpdateEyeLightIntensity(int mutationIndex)
     {
         int maxIndex = 6;
-        eyeLight1.intensity = Mathf.Lerp(eyeLight1.intensity, maxEyeLightIntensity,
-         (float)mutationIndex / maxIndex);
-        eyeLight2.intensity = Mathf.Lerp(eyeLight2.intensity, maxEyeLightIntensity,
-         (float)mutationIndex / maxIndex);
+        eyeLight1.intensity = Mathf.Lerp(
+            eyeLight1.intensity,
+            maxEyeLightIntensity,
+            (float)mutationIndex / maxIndex
+        );
+        eyeLight2.intensity = Mathf.Lerp(
+            eyeLight2.intensity,
+            maxEyeLightIntensity,
+            (float)mutationIndex / maxIndex
+        );
     }
-   
 }
